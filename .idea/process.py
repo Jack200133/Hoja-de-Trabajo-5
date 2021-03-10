7e318637c639def2ed26d4363ab398adc97ceaca
Jack200133 <car20593@uvg.edu.gt>
2021-03-09 22:31:43 -0600

    Update process.py
    "Comentarios y ultimos arreglos de el env"

--- .idea/process.py
+++ .idea/process.py
@@ -12,48 +12,56 @@ import math
 
 
 def process(env, name, Ram_need, cant_inst, CPU, wait_time, RAM):
+    global totaldia
+    totaldia = 0
+    llegada = env.now
     run = True
     while run:
         yield RAM.get(Ram_need)
-        print(' %s esta listo para empezar, con un tienpo de %s, ram = %s' % (
+        print(' %s esta listo para empezar, con un tiempo de %s, ram = %s' % (
             name, env.now,  Ram_need))
         while cant_inst > 0:
 
             with CPU.request() as req:
 
                 yield req
-                yield env.timeout(3)
-                cant_inst = cant_inst - 3
+                yield env.timeout(1)
+                cant_inst = cant_inst - 3  # cantidad de instrucciones x unidad de tiempo
                 if(cant_inst < 0):
                     cant_inst = 0
-                print(' %s Hace 3 procesos, con un tienpo de %s y queda con %s penientes' %
+                print(' %s Hace 3 procesos, con un tiempo de %s y queda con %s penientes' %
                       (name, env.now, cant_inst))
+            # random por si se va aespera o vuelve al CPU
             feo = random.randint(1, 3)
             if(feo == 1):
                 with wait_time.request() as req:
                     yield req
                     yield env.timeout(1)
-                    print(' %s entra a espera, con un tienpo de %s' % (
+                    print(' %s entra a espera, con un tiempo de %s' % (
                         name, env.now))
-        RAM.put(Ram_need)
-        print(' %s FINALIZADO, finaliza con un tienpo de %s' %
-              (name, env.now))
+        RAM.put(Ram_need)  # regresa la ram que uso este proceso
+        tiempo = env.now - llegada
+        print(' %s FINALIZADO, finaliza con un tiempo de %s' %
+              (name, tiempo))
+        totaldia = totaldia + tiempo
         run = False
 
 
 env = simpy.Environment()  # crea el ambiente de simulacion
-RAM = simpy.Container(env, init=100, capacity=100)
-CPU = simpy.Resource(env, capacity=3)
-wait = simpy.Resource(env, capacity=25)
+RAM = simpy.Container(env, init=100, capacity=100)  # capacidad del RAM
+CPU = simpy.Resource(env, capacity=1)  # cantidad de CPUS
+# cantidad de colas para la espera a CPU
+wait = simpy.Resource(env, capacity=3)
+cantidadpro = 50  # cantidad de procesos
+random.seed(10)  # seed del random
 
-random.seed(10)
+for i in range(cantidadpro):
+    Cantint = random.randint(1, 10)  # cantidad de uinstrucciones x proceso
 
-for i in range(50):
-    Cantint = random.randint(1, 10)
-
-    Ram_need = (random.expovariate(1/10) % 10+1)
+    Ram_need = (random.expovariate(1/10) % 10+1)  # cantidad de ram x proceso
 
     env.process(process(env, 'Proceso %d' %
                         i, round(Ram_need), Cantint, CPU, wait, RAM))
 
 env.run()
+print('Tiempo promedio ', totaldia/cantidadpro)
